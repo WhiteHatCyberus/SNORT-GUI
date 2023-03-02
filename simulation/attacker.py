4458f449342b82792ec9a2a04909c6b13b8bc038
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2023-03-02 18:08:19 +0530

    Update attacker.py

--- simulation/attacker.py
+++ simulation/attacker.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import os
+import subprocess
 from tkinter import messagebox
 
 # Create the main window
@@ -7,7 +7,11 @@ root = tk.Tk()
 root.title("Attack Simulation")
 root.resizable(False, False)
 
-# Create the menu bar
+# Create the label for the attack type
+attack_label = tk.Label(root, text="Select Attack Type:")
+attack_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+# Create the menu bar for the attack type options
 menu_bar = tk.Menu(root)
 attack_menu = tk.Menu(menu_bar, tearoff=0)
 attack_menu.add_command(label="Probe")
@@ -17,24 +21,28 @@ attack_menu.add_command(label="R2L")
 menu_bar.add_cascade(label="Select Attack Type", menu=attack_menu)
 root.config(menu=menu_bar)
 
-# Create the text field
+# Create the label for the target IP
 ip_label = tk.Label(root, text="Enter Target IP:")
-ip_label.pack()
+ip_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+# Create the text field for the target IP
 ip_entry = tk.Entry(root)
-ip_entry.pack()
+ip_entry.grid(row=1, column=1, padx=5, pady=5)
 
 # Define the function to run the ping command
 def run_ping():
     target_ip = ip_entry.get()
-    response = os.system("ping -c 4 " + target_ip + " | grep '0% packet loss'")
-    if response == 0:
+    ping_process = subprocess.Popen(['ping', '-c', '4', target_ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ping_output, ping_error = ping_process.communicate()
+    ping_output_str = ping_output.decode('utf-8')
+    if "0% packet loss" in ping_output_str:
         messagebox.showinfo("Success", "Ping to " + target_ip + " was successful!")
     else:
         messagebox.showerror("Failure", "Ping to " + target_ip + " failed.")
 
 # Create the button to run the ping command
 ping_button = tk.Button(root, text="Ping", command=run_ping)
-ping_button.pack()
+ping_button.grid(row=2, column=0, padx=5, pady=5)
 
 # Run the main loop
 root.mainloop()
